88c39de5aef864210c44cb467b576a2be82478ad
Matthew L. Bowser <matt_bowser@fws.gov>
2022-05-09 11:28:19 -0800

    share setup options and fix a caption

--- 2022/01/compile.R
+++ 2022/01/compile.R
@@ -1,7 +1,7 @@
 library(rmarkdown)
 
 main <- "AKES_newsletter_2022_01.Rmd"
-articles <- c("chokecherry_midge.Rmd", "corrections.Rmd", "Miller_Creek_inventory.Rmd")
+articles <- c("chokecherry_midge.Rmd", "corrections.Rmd")
 
 rmarkdown::render(input = main, output_format = "all")
 
---
+++ share/setup.R
@@ -0,0 +1,15 @@
+knitr::opts_chunk$set(
+  comment = "#>", 
+  echo = FALSE,
+  message=FALSE, 
+  warning=FALSE,
+  fig.align='center',
+  out.width='50%'
+)
+
+options(knitr.table.format = "pipe")
+
+pream <- readLines("../../share/preamble_Rmd_pre_sub.tex")
+issueinfo <- paste0("Volume ", params$volume, ", Issue ", params$issue, ", ", format(Sys.time(), '%B %Y'))
+pream <- gsub("issueinfo", issueinfo, pream)
+write(pream, file = "../../share/preamble_Rmd.tex")
