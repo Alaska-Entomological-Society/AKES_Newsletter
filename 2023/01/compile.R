0f4aeb1dea91eaea450a30817f7c26a6f90d1dcb
Matt Bowser <matt_bowser@fws.gov>
2023-03-17 11:35:30 -0800

    Get pdf export working

---
+++ 2023/01/compile.R
@@ -0,0 +1,12 @@
+library(rmarkdown)
+
+main <- "AKES_newsletter_2023_01.Rmd"
+
+rmarkdown::render(input = main, output_format = "all")
+
+for (this_article in articles){
+  rmarkdown::render(input = this_article, output_format = "all")
+}
+
+
+
